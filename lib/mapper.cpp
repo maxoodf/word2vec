3f2445be1440ef03530284c81f2fb5c5c978fc8b
maxoodf <max.fomitchev@gmail.com>
2017-05-18 14:18:13 +0300

    fileMapper_t - extended error info

--- lib/mapper.cpp
+++ lib/mapper.cpp
@@ -33,26 +33,26 @@ namespace w2v {
         // open file
         m_fd = ::open(m_fileName.c_str(), m_wrFlag?(O_RDWR | O_CREAT):O_RDONLY, 0600);
         if (m_fd < 0) {
-            std::string err = std::string("fileMapper: ") + std::strerror(errno);
+            std::string err = std::string("fileMapper: ") + _fileName + " - " + std::strerror(errno);
             throw std::runtime_error(err);
         }
 
         // get file size
         struct stat fst;
         if (fstat(m_fd, &fst) < 0) {
-            std::string err = std::string("fileMapper: ") + std::strerror(errno);
+            std::string err = std::string("fileMapper: ") + _fileName + " - " + std::strerror(errno);
             throw std::runtime_error(err);
         }
 
         if (!m_wrFlag) {
             if (fst.st_size <= 0) {
-                throw std::runtime_error("fileMapper: file is empty, nothing to read");
+                throw std::runtime_error(std::string("fileMapper: file ") + _fileName + " is empty, nothing to read");
             }
 
             m_size = fst.st_size;
         } else {
             if (ftruncate(m_fd, m_size) == -1) {
-                std::string err = std::string("fileMapper: ") + std::strerror(errno);
+                std::string err = std::string("fileMapper: ") + _fileName + " - " + std::strerror(errno);
                 throw std::runtime_error(err);
             }
         }
@@ -62,7 +62,7 @@ namespace w2v {
                                                  m_wrFlag?(PROT_READ | PROT_WRITE):PROT_READ , MAP_SHARED,
                                                  m_fd, 0));
         if (m_data.rwData == static_cast<char *>(MAP_FAILED)) {
-            std::string err = std::string("fileMapper: ") + std::strerror(errno);
+            std::string err = std::string("fileMapper: ") + _fileName + " - " + std::strerror(errno);
             throw std::runtime_error(err);
         }
     }
